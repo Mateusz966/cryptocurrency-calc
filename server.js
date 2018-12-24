d2aaf2336abe02a12f300dadcd401c1a2ee419a7
Mateusz966 <37120337+Mateusz966@users.noreply.github.com>
2018-12-24 09:34:53 +0100

    Configure server

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const express = require('express');
 const request = require('request');
 const fs = require('fs');
 const cron = require('node-cron');
+const path = require('path');
 
 const port = 8080;
 const apiServer = 'https://www.coincalculators.io/api/allcoins.aspx?hashrate=100000000';
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static(path.join(__dirname,'/')));
 
 app.listen(port, () => {
   console.log(`server running at ${port} `);
